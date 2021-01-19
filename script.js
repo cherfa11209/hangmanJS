cca5fbaa9b5df79825439fe26852b00b512d6f16
cherfa11209 <sherifcherfa@gmail.com>
2021-01-19 19:48:32 +0000

    I linked all the HTML elementss to javascript

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const letters = Array.from(document.querySelectorAll(".letter"));
+const bar = document.getElementById("bar");
+const enter = document.getElementsByClassName("enter")[0];
+const choices = Array.from(document.querySelectorAll(".choice"));
+const secret = document.getElementsByClassName("secret_word")[0];
 
+console.log(secret)
