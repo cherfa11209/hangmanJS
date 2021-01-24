06b17c3bd44a0c337ecf50574b0db9a7e90163e9
skylar cherfa <sherifcherfa@gmail.com>
2021-01-24 21:16:56 +0000

    adding functions to my JS file

--- script.js
+++ script.js
@@ -12,32 +12,38 @@ const rightarm = document.getElementById('rightarm');
 const leftarm = document.getElementById('leftarm');
 const rightleg = document.getElementById('rightleg');
 const leftleg = document.getElementById('leftleg');
+const bg = document.getElementsByClassName('display')[0]
 let secretword = 0;
 let secretwordarray = [];
 let user_guess = 0;
+let chances = 8;
 
 function computer_chooses(){
     computer_choice = Math.floor(Math.random()*animals.length)
     return computer_choice
 }
 
+function updateswa(){
+    secret.innerHTML = secretwordarray;
+}
+
 animal_btn.addEventListener('click', function(){
     secretword = animals[computer_chooses()]
     populate();
-    secret.innerHTML = secretwordarray;
+    updateswa()
     console.log(secretword)
 })
 
 city_btn.addEventListener('click', function(){
     secretword = cities[computer_chooses()]
     populate();
-    secret.innerHTML = secretwordarray;
+    updateswa()
 })
 
 food_btn.addEventListener('click', function(){
     secretword = foods[computer_chooses()]
     populate();
-    secret.innerHTML = secretwordarray;
+    updateswa()
     console.log(secretword)
 })
 
@@ -58,10 +64,48 @@ letters.forEach((letter) =>{
 
 function check_for_match(value){
     if(secret_arr.includes(value)){
-        console.log(secret_arr[value])
+        console.log(secret_arr)
+        let match = secret_arr.indexOf(value)
+        secretwordarray[match] = value
+        secret_arr[match] = '_'
+        updateswa()
+    } else {
+        chances -= 1;
+        check_chances(chances)
     }
 }
 
+function check_chances(chance){
+    switch(chance) {
+        case chance = 7:
+        rope.innerHTML = "|"
+        break;
+        case chance = 6:
+        head.innerHTML = 'o'
+        break;
+        case chance = 5:
+        head.innerHTML = '|'
+        break;
+        case chance = 4:
+        head.innerHTML = '/'
+        break;
+        case chance = 3:
+        head.innerHTML = '\''
+        break;
+        case chance = 2:
+        head.innerHTML = '/'
+        break;
+        case chance = 1:
+        head.innerHTML = '\~'
+        break;
+        case chance = 0:
+        hea
+        break;
+
+        default:
+          console.log("ya")
+      }
+}
 
 
 
